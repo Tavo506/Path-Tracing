31608782af1ecbc51d8e0854cf527c560db8bdd7
Gustavo Blanco Alfaro <32990052+Tavo506@users.noreply.github.com>
2020-06-16 12:23:18 -0600

    Main

--- main.py
+++ main.py
@@ -1,18 +1,79 @@
-from turtle import *
+import numpy as np 
+import pygame
+import random
+from PIL import Image
+from Point import *
+import rt
+import math
+import threading
+ 
 
-title("Path Tracing")
-screensize(550,550)
-bgcolor("#000000")
+def getFrame():
+    # grabs the current image and returns it
+    pixels = np.roll(px,(1,2),(0,1))
+    return pixels
 
-hideturtle()
 
-color("#FFFFFF")
-down()
+#pygame stuff
+h,w=550,550
+border=50
 
-for i in range(100):
-    if(i > 50):
-        forward(-1)
-    else:
-        forward(1)
 
-done()
+pygame.init()
+screen = pygame.display.set_mode((w+(2*border), h+(2*border)))
+pygame.display.set_caption("Path Tracing")
+done = False
+clock = pygame.time.Clock()
+
+
+#init random
+random.seed()
+
+#image setup
+i = Image.new("RGB", (500, 500), (0, 0, 0) )
+px = np.array(i)
+
+#reference image for background color
+im_file = Image.open("fondo.png")
+ref = np.array(im_file)
+
+#light positions
+sources = [ Point(195, 200), Point( 294, 200) ]
+
+#light color
+light = np.array([1, 1, 0.75])
+#light = np.array([1, 1, 1])
+
+#warning, point order affects intersection test!!
+segments = [
+            ([Point(180, 135), Point(215, 135)]), 
+            ([Point(285, 135), Point(320, 135)]),
+            ([Point(320, 135), Point(320, 280)]),
+            ([Point(320, 320), Point(320, 355)]),
+            ([Point(320, 355), Point(215, 355)]),
+            ([Point(180, 390), Point(180, 286)]),
+            ([Point(180, 286), Point(140, 286)]),
+            ([Point(320, 320), Point(360, 320)]),
+            ([Point(180, 250), Point(180, 135)]),
+            ]
+
+
+
+#main loop
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+
+        # Clear screen to white before drawing 
+        screen.fill((255, 255, 255))
+
+        # Get a numpy array to display from the simulation
+        npimage=getFrame()
+
+        # Convert to a surface and splat onto screen offset by border width and height
+        surface = pygame.surfarray.make_surface(npimage)
+        screen.blit(surface, (border, border))
+
+        pygame.display.flip()
+        clock.tick(60)
