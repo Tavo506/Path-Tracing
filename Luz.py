0aff462da7d574892593e20c372343191df07615
Gustavo Blanco Alfaro <32990052+Tavo506@users.noreply.github.com>
2020-06-26 18:23:18 -0600

    Documentación

--- Luz.py
+++ Luz.py
@@ -17,17 +17,20 @@ class Luz:
 		self.rayo.pensize(2)
 		self.rayo.setundobuffer(None)
 
+	#Mueve el rayo de vuelta al origen
 	def goHome(self):
 		self.rayo.up()
 		self.rayo.setpos(self.x, self.y)
 		#self.rayo.setheading(360)
 		self.rayo.down()
 
+	#Pone la direccion del rayo
 	def rotar(self, grados):
 		self.rayo.up()
 		self.rayo.setheading(grados)
 		self.rayo.down()
 
+	#Cambial al color y se mueve un pixel
 	def go(self, color):
 		self.rayo.pencolor(color)
 		self.rayo.forward(1)
@@ -44,48 +47,64 @@ class Luz:
 	def getDir(self):
 		return self.rayo.heading()
 
+	#Retorna el valor de rebote
 	def puedeRebotar(self):
 		return self.primerRebote
 
 	def setRebote(self, estado):
 		self.primerRebote = estado
 
+	#Calcula la rotación según el ángulo y la pared con la que está chocando
 	def cambioDireccion(self, orientacion, dir):
 		self.rayo.backward(1)
+
 		if(orientacion == 1):	#pared vertical
+			#Obtiene el ángulo interno que no conocemos y el ángulo a rotar es el doble de este
 			
-			if(90 > dir > 0):
-				x = 90 - dir
-				angulo = (x*2)
-				self.rayo.left(angulo)
+			if(90 > dir > 0):				#  	\  |
+				x = 90 - dir 				#  	 \ |
+				angulo = (x*2) 				#  	  \|
+				self.rayo.left(angulo) 		#  	  /|
+											#	 v |
 
-			elif(180 > dir > 90):
-				x = 90 - dir
-				angulo = (-x*2)
-				self.rayo.right(angulo)
+			elif(180 > dir > 90):			#  |  /
+				x = 90 - dir 				#  | /
+				angulo = (-x*2) 			#  |/
+				self.rayo.right(angulo) 	#  |\
+ 											#  | v
 
-			elif(270 > dir > 180):
-				x = 90 - dir
-				angulo = (-x*2)
-				self.rayo.right(angulo)
+			elif(270 > dir > 180):			#  | ^ 
+				x = 90 - dir 				#  |/  
+				angulo = (-x*2) 			#  |\
+				self.rayo.right(angulo) 	#  | \
+											#  |  \ 
 
-			elif(360 > dir > 270):
-				x = 90 - (360 - dir)
-				angulo = (x*2)
-				self.rayo.right(angulo)
+			elif(360 > dir > 270):			#  	^ |
+				x = 90 - (360 - dir) 		#  	 \|
+				angulo = (x*2) 				#  	 /|
+				self.rayo.right(angulo) 	#  	/ |
+											#  /  |
+
 		else:					#pared horizontal
-			if(90 > dir > 0):
-				angulo = (-dir*2)
-				self.rayo.left(angulo)
 
-			elif(180 > dir > 90):
-				angulo = (dir*2)
-				self.rayo.right(angulo)
+			#Siempre es el doble de la direccion actual (rotándolo a donde corresponda)
 
-			elif(270 > dir > 180):
-				angulo = (dir*2)
-				self.rayo.right(angulo)
+			if(90 > dir > 0):				#	  \
+				angulo = (-dir*2)			#	   \	^
+				self.rayo.left(angulo)		#		\  /
+											#________\/_________
 
-			elif(360 > dir > 270):
-				angulo = (-dir*2)
-				self.rayo.left(angulo)
+			elif(180 > dir > 90):			#	  		 /
+				angulo = (dir*2)			#	   ^    /
+				self.rayo.right(angulo)		#		\  /
+											#________\/_________
+
+			elif(270 > dir > 180):			#___________________
+				angulo = (dir*2)			#		 /\
+				self.rayo.right(angulo)		#		v  \
+											#			\
+
+			elif(360 > dir > 270):			#___________________
+				angulo = (-dir*2)			#		 /\
+				self.rayo.left(angulo)		#		/  v
+											#	   /
--- main.py
+++ main.py
@@ -6,25 +6,29 @@ from Luz import *
 import threading
 
 def getColor(x, y, i, ri, gi, bi):
-    i = i // 3
+    i = i // 3      #Calcula la intencidad
 
-    colorRGB = px[int(x),int(y)]
+    colorRGB = px[int(x),int(y)]    #Obtiene el color en el pixel actual
 
-    R = colorRGB[0]
+    R = colorRGB[0]     #Separa los tres colores
     G = colorRGB[1]
     B = colorRGB[2]
 
+    #Crea el número hexadecimal del color actual para luego compararlo con las paredes y cristales
     colorH = "#" + str(hex(R).split('x')[-1]) + str(hex(G).split('x')[-1]) + str(hex(B).split('x')[-1])
 
+    #Realiza los ajustes del color con intencidad y tono
     R = R - i + ri
     G = G - i + gi
     B = B - i + bi
 
-    if(R >= 16):
+    #Calcula cada color en hexadecimal tomando los casos posibles
+
+    if(R >= 16):            #Si es mayor a 15 queda igual
         R = hex(R).split('x')[-1]
-    elif(R >= 0):
+    elif(R >= 0):           #Si es menor coloca un 0 al frente y concatena
         R = "0" + hex(R).split('x')[-1]
-    else:
+    else:                   #Si es un número negativo entonces debe ser cero
         R = "00"
 
     if(G >= 16):
@@ -41,14 +45,14 @@ def getColor(x, y, i, ri, gi, bi):
     else:
         B = "00"
 
-    colorRGB = "#" + R + G + B
+    colorRGB = "#" + R + G + B      #Concatena los colores como un color hexadecimal
 
     return [colorH, colorRGB]
 
-
+#Pregunta si 4 pixeles arriba del actual sigue siendo pared, de ser así quiere decir que es una pared vertical
 def calcularDireccion(x, y):
     color = getColor(x, y+4, 0, 0, 0, 0)
-    if(color[0] in ["#d7d4cf", "#812b2b", "#6d6d6d"]):
+    if(color[0] in ["#d7d4cf", "#812b2b", "#6d6d6d"]): #Paredes
         return 1    #vertical
     else:
         return 2    #horizontal
@@ -72,7 +76,7 @@ def pathtracing(Rayo, direcciones, rl, gl, bl):
         Rayo.rotar(dir) #Rota el rayo a la dirección
         color = ["",""]
 
-        ri = rl
+        ri = rl     #Vuelve a setear los colores al de la fuente de luz
         gi = gl
         bi = bl
 
@@ -91,47 +95,44 @@ def pathtracing(Rayo, direcciones, rl, gl, bl):
             except:
                 update()
 
-            if(color[1] == "#000000"):
+            if(color[1] == "#000000"):  #Si el color es negro deja de calcular más
                 break
 
             Rayo.go(color[1])  #Se mueve uno y pinta
 
             if(color[0] in ["#d7d4cf", "#6d6d6d"]): #Si el color es un muro:
-                if(Rayo.puedeRebotar()):
-                    Rayo.setRebote(False)
-                    orientacion = calcularDireccion(x, y)
-                    Rayo.cambioDireccion(orientacion, dir)
+                if(Rayo.puedeRebotar()):    #Si aún no rebota...
+                    Rayo.setRebote(False)   #Pone que ya rebotó
+                    orientacion = calcularDireccion(x, y)   #Calcula si la pared es vertical u horizontal
+                    Rayo.cambioDireccion(orientacion, dir)  #Calcula la rotación
                     Rayo.go(color[1])
                 else:
                     break
 
-            elif(color[0] == "#ec7ca1"):
-                ri = 20
+            elif(color[0] == "#ec7ca1"):    #Cristal rosado
+                ri = 20                     #Lo tiñe de rosa
                 gi = -10
                 bi = -10
-            elif(color[0]=="#5ad6d0"):
-            	ri=-10
+            elif(color[0]=="#5ad6d0"):      #Cristal celeste
+            	ri=-10                      #Lo tiñe de celeste
             	gi=-10
             	bi=20
 
-            elif(color[0] in ["#812b2b", ""] ):
+            elif(color[0] in ["#812b2b"]):  #Pared roja
                 if(Rayo.puedeRebotar()):
                     Rayo.setRebote(False)
                     orientacion = calcularDireccion(x, y)
                     Rayo.cambioDireccion(orientacion, dir)
                     Rayo.go(color[1])
 
+                                        #Lo tiñe de rojo y rebota
                     ri = 30
                     gi = -10
                     bi = -10
                 else:
                     break
 
-
-        
-
-#print("#" + str(hex(109).split('x')[-1]) + str(hex(109).split('x')[-1]) + str(hex(109).split('x')[-1]))
-
+#Crea una lista con las direcciones y crea copias
 direcciones = []
 for i in range(360):
     direcciones.append(i)
@@ -143,6 +144,7 @@ direcciones4 = direcciones[:]
 direcciones5 = direcciones[:]
 direcciones6 = direcciones[:]
 
+#Carga la imagen y guarda los pixeles en un array
 im_file = Image.open("fondo.png")
 px = im_file.load()
 
@@ -150,8 +152,9 @@ title("Path Tracing 4K")
 setup(500, 500)
 bgcolor(0,0,0)
 setworldcoordinates(0, 500, 500, 0)
-tracer(0,10000)
+tracer(0,10000) #velocidad de ejecución y actualización de la pantalla
 
+#Crea los rayos de luz para cada fuente
 Rayo1 = Luz(180, 305)
 Rayo2 = Luz(180, 305)
 
@@ -167,37 +170,29 @@ Rayo7 = Luz(42, 429)
 Rayo8 = Luz(153,154)
 
 #thread setup
-t1 = threading.Thread(target = pathtracing, args=(Rayo1,direcciones1,5,5,-5,)) # f being the function that tells how the ball should move
-t1.setDaemon(True) # Alternatively, you can use "t.daemon = True"
+                                                # La luz, lista dir, tono de luz
+t1 = threading.Thread(target = pathtracing, args=(Rayo1,direcciones1,5,5,-5,))
 t1.start()
 
-
-t2 = threading.Thread(target = pathtracing, args=(Rayo2,direcciones1,5,5,-5,)) # f being the function that tells how the ball should move
-t2.setDaemon(True) # Alternatively, you can use "t.daemon = True"
+t2 = threading.Thread(target = pathtracing, args=(Rayo2,direcciones1,5,5,-5,))
 t2.start()
 
-t3 = threading.Thread(target = pathtracing, args=(Rayo3,direcciones2,5,5,-5,)) # f being the function that tells how the ball should move
-t3.setDaemon(True) # Alternatively, you can use "t.daemon = True"
+t3 = threading.Thread(target = pathtracing, args=(Rayo3,direcciones2,5,5,-5,))
 t3.start()
 
-t4 = threading.Thread(target = pathtracing, args=(Rayo4,direcciones3,5,5,-5,)) # f being the function that tells how the ball should move
-t4.setDaemon(True) # Alternatively, you can use "t.daemon = True"
+t4 = threading.Thread(target = pathtracing, args=(Rayo4,direcciones3,5,5,-5,))
 t4.start()
 
-t5 = threading.Thread(target = pathtracing, args=(Rayo5,direcciones4,5,5,-5,)) # f being the function that tells how the ball should move
-t5.setDaemon(True) # Alternatively, you can use "t.daemon = True"
+t5 = threading.Thread(target = pathtracing, args=(Rayo5,direcciones4,5,5,-5,))
 t5.start()
 
-t6 = threading.Thread(target = pathtracing, args=(Rayo6,direcciones4,5,5,-5,)) # f being the function that tells how the ball should move
-t6.setDaemon(True) # Alternatively, you can use "t.daemon = True"
+t6 = threading.Thread(target = pathtracing, args=(Rayo6,direcciones4,5,5,-5,))
 t6.start()
 
-t7 = threading.Thread(target = pathtracing, args=(Rayo7,direcciones5,0,-10,-10,)) # f being the function that tells how the ball should move
-t7.setDaemon(True) # Alternatively, you can use "t.daemon = True"
+t7 = threading.Thread(target = pathtracing, args=(Rayo7,direcciones5,0,-10,-10,))
 t7.start()
 
-t8 = threading.Thread(target = pathtracing, args=(Rayo8,direcciones6,5,5,-5,)) # f being the function that tells how the ball should move
-t8.setDaemon(True) # Alternatively, you can use "t.daemon = True"
+t8 = threading.Thread(target = pathtracing, args=(Rayo8,direcciones6,5,5,-5,))
 t8.start()
 
 done()
